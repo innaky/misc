37a058b08f33cfd89df51c3dd4232e0a9e13e5c2
Innaky <innaky@protonmail.com>
2019-02-09 21:23:17 -0400

    Copy the iso information.

--- build_iso.sh
+++ build_iso.sh
@@ -1,3 +1,8 @@
 function mount-iso () {
     mount -o loop iso mount-iso-dir
 }
+
+function extract-iso () {
+    rsync -a -H --exclude=TRANS.TBL mount-iso-dir point-to-extract
+    umount mount-iso-dir
+}
