86847955df77629852aa1d8b2ed8bed036938f73
Innaky <innaky@protonmail.com>
2019-02-12 23:55:40 -0400

    adding small function for building iso image.

--- build_iso.sh
+++ build_iso.sh
@@ -6,3 +6,14 @@ function extract-iso () {
     rsync -a -H --exclude=TRANS.TBL mount-iso-dir point-to-extract
     umount mount-iso-dir
 }
+
+function mount-chroot () {
+    for i in /etc/resolv.conf /etc/hosts /etc/hostname; do
+	cp -pv $i localchroot/etc/
+    done
+
+    mount -t proc proc explained_iso/proc
+    mount -t sysfs sysfs explained_iso/sys
+    mount -o bind /dev explained_iso/dev
+    mount -t devpts pts explained_iso/dev/pts
+}
