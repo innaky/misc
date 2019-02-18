1626d0217e58f517da4b79ab81d283d540ea1772
Innaky <innaky@protonmail.com>
2019-02-18 18:28:27 -0400

    More recursion functions :D, with normal and tail-recursion.

--- recursion.erl
+++ recursion.erl
@@ -1,5 +1,5 @@
 -module(recursion).
--export([fact/1, factmatch/1, my_length/1, tail_fact/1, tail_len/1, repeat/2, tail_repeat/2]).
+-export([fact/1, factmatch/1, my_length/1, tail_fact/1, tail_len/1, repeat/2, tail_repeat/2, reverse/1, tail_reverse/1, tail_reverser/1, sublist/2, tail_sublist/2, zip/2, tail_zip/2, partition/4, quicksort/1]).
 
 fact(X) when X == 0 ->
     1;
@@ -43,3 +43,67 @@ tail_repeat(_, Times, Acc) when Times == 0 ->
     Acc;
 tail_repeat(Num, Times, Acc) ->
     tail_repeat(Num, Times-1, [Num|Acc]).
+
+reverse([]) ->
+    [];
+reverse([H|T]) ->
+    reverse(T)++[H].
+
+tail_reverse(List) ->
+    tail_reverse(List, []).
+tail_reverse(List, Acc) when List == [] ->
+    Acc;
+tail_reverse([H|T], Acc) ->
+    tail_reverse(T, [H|Acc]).
+
+tail_reverser(List)->
+    tail_reverser(List, []).
+tail_reverser([], Acc) ->
+    Acc;
+tail_reverser([H|T],Acc) ->
+    tail_reverser(T, [H|Acc]).
+
+sublist(_, 0) ->
+    [];
+sublist([H|T], Num) when Num >= 0 ->
+    [H]++sublist(T, Num-1).
+
+tail_sublist(List, Num) ->
+    tail_sublist(List, Num, []).
+tail_sublist(_, 0, Acc) ->
+    Acc;
+tail_sublist([], _, Acc) ->
+    Acc;
+tail_sublist([H|T], Num, Acc) when Num > 0 ->
+    tail_sublist(T, Num-1, Acc++[H]).
+
+zip([], _) ->
+    [];
+zip(_, []) ->
+    [];
+zip([Hx|Tx], [Hy|Ty]) ->
+    [{Hx, Hy}]++zip(Tx, Ty).
+
+tail_zip(Flist, Slist) ->
+    tail_zip(Flist, Slist, []).
+tail_zip(_, [], Acc) ->
+    Acc;
+tail_zip([], _, Acc) ->
+    Acc;
+tail_zip([Hx|Tx], [Hy|Ty], Acc) ->
+    [{Hx,Hy}]++tail_zip(Tx, Ty, Acc).
+
+partition(_, [], Smaller, Larger) ->
+    {Smaller, Larger};
+partition(Pivot, [H|T], Smaller, Larger) ->
+    if H >= Pivot ->
+    partition(Pivot, T, [H|Smaller], Larger);
+       H < Pivot ->
+    partition(Pivot, T, Smaller, [H|Larger])
+    end.
+
+quicksort([]) ->
+    [];
+quicksort([Pivot|T]) ->
+    {Smaller, Larger} = partition(Pivot, T, [], []),
+    quicksort(Smaller) ++ [Pivot] ++ quicksort(Larger).
