3bf1511fec2d929c4c7a7b915b63971eb03462a2
Innaky <innaky@protonmail.com>
2019-02-19 19:16:35 -0400

    logic and basic form to insert elements in a list :D.

--- variatefunc.erl
+++ variatefunc.erl
@@ -1,12 +1,12 @@
 -module(variatefunc).
--export([greet/2, head/1, second/1, other_second/1, third_second/1, valid_time/1]).
+-export([greet/2, head/1, second/1, other_second/1, third_second/1, valid_time/1, insert/2]).
 
 greet(male, Name) ->
     io:format("Hello, Mr. ~s!~n", [Name]);
 greet(female, Name) ->
     io:format("Hello, Mrs. ~s!~n", [Name]);
 greet(_, Name) ->
-    io:format("Hello, ~s~n!", [Name]).
+    io:format("Hello, ~s!~n", [Name]).
 
 head([H|_]) ->
     H.
@@ -28,3 +28,13 @@ valid_time({Date = {Y, M, D}, Time = {H, Min, S}}) ->
     io:format("The time tuple (~p) indicates: ~p:~p:~p.~n", [Time, H, Min, S]);
 valid_time(_) ->
     io:format("Stop feeding we wrong data!~n").
+
+insert(X, []) ->
+    [X];
+insert(X, Set) ->
+    case lists:member(X, Set) of
+	true ->
+	    Set;
+	false ->
+	    [X|Set]
+end.
