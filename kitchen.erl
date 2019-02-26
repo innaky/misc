2e27ca23b1390a4aefe7024f4e928ff5ffc8acbf
Innaky <innaky@protonmail.com>
2019-02-25 23:01:23 -0400

    The server have a structure of questions match, the clients ask about this (previously defined in the server structure). receive activate the answers. ! activate the questions.

--- kitchen.erl
+++ kitchen.erl
@@ -1,6 +1,7 @@
 -module(kitchen).
 -compile(export_all).
 
+%% return only not found, this function is very poor
 fridge1() ->
     receive
 	{From, {store, _Food}} ->
@@ -11,4 +12,60 @@ fridge1() ->
 	    fridge1();
 	terminate ->
 	    ok
+    end.
+
+%% more versatile and recursive function
+%% server
+
+start2(FoodList) ->
+    spawn(kitchen, fridge2, [[FoodList]]).
+
+fridge2(FoodList) ->
+    receive
+	%% match first receive
+	{From, {store, Food}} ->
+            %% first answer
+	    From ! {self(), {ok, Food}},
+	    fridge2([Food|FoodList]);
+	{From, {list_all, ok}} ->
+	    From ! {self(), {ok, FoodList}},
+	    fridge2(FoodList);
+        %% match other receive
+	{From, {take, Food}} ->
+            %% conditional answer
+	    case lists:member(Food, FoodList) of
+		true ->
+		    From ! {self(), {ok, Food}},
+		    fridge2(lists:delete(Food, FoodList));
+		false ->
+		    From ! {self(), not_found},
+		    fridge2(FoodList)
+	    end;
+	terminate ->
+	    ok
+    end.
+
+%%% clients
+%% store a food
+store(Pid, Food) ->
+    Pid ! {self(), {store, Food}},
+    receive
+	{Pid, Msg} ->
+	    Msg
+    end.
+
+%% take a food
+take(Pid, Food) ->
+    Pid ! {self(), {take, Food}},
+    receive
+	{Pid, Msg} ->
+	    Msg
+    end.
+
+%% list all food
+list_products(Pid) ->
+    Pid ! {self(), {list_all, ok}},
+    receive
+	{Pid, Msg} ->
+	    Msg
     end.
