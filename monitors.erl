11aa02af97c6f134628dca5287b2dc75a66ecdb2
Innaky <innaky@protonmail.com>
2019-03-01 01:36:28 -0400

    Adding a named processes, If you kill the process a new one instantly came back to solve our problems :).

--- monitors.erl
+++ monitors.erl
@@ -4,6 +4,8 @@
 start_critic() ->
     spawn(?MODULE, critic, []).
 
+%% normal runnig but if exit send critic with {Pid, Criticism} pattern, the function
+%% timeout before 2 seconds.
 judge(Pid, Band, Album) ->
     Pid ! {self(), {Band, Album}},
     receive
@@ -25,3 +27,43 @@ critic() ->
 	    From ! {self(), "They are terrible!"}
     end,
     critic().
+
+%% run a new logic with a name process and a trap_exit flag using
+%% a patern matching with the exit message.
+start_critic2() ->
+    spawn(?MODULE, restarter, []).
+
+restarter() ->
+    process_flag(trap_exit, true),
+    Pid = spawn_link(?MODULE, critic2, []),
+    register(critic, Pid),
+    receive
+	{'EXIT', Pid, normal} ->
+	    ok; % not a crash
+	{'EXIT', Pid, shutdown} ->
+	    ok; % manual kill, not a crash
+	{'EXIT', Pid, _} ->
+	    restarter()
+    end.
+
+judge2(Band, Album) ->
+    Ref = make_ref(),
+    critic ! {self(), Ref, {Band, Album}},
+    receive
+	{Ref, Criticism} -> Criticism
+    after 2000 ->
+	    timeout
+    end.
+
+critic2() ->
+    receive
+	{From, Ref, {"Rage Against the Turing Machine", "Unit Testify"}} ->
+	    From ! {Ref, "They are great!"};
+	{From, Ref, {"System of a Downtime", "Memoize"}}  ->
+	    From ! {Ref, "They're not Johnny Crash but they're good."};
+	{From, Ref, {"Johnny Crash", "The Token Ring of Fire"}} ->
+	    From ! {Ref, "Simply incredible."};
+	{From, Ref, {_Band, _Album}} ->
+	    From ! {Ref, "They are terrible!"}
+    end,
+    critic2().
