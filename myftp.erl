aaa3fe4a463d7d9761973b0b4841d13fabfd50f6
Innaky <innaky@protonmail.com>
2019-04-04 22:40:55 -0400

    Adding more documentation for execution in the Erlang shell, from the client get a file of the server %% The remote pc is: server@innaky % > erl -sname client -setcookie abc % > c(myftp). % {ok,myftp} % > > myftp:get_file(server@innaky, "monitors.erl"). % ok

--- myftp.erl
+++ myftp.erl
@@ -4,3 +4,10 @@
 get_file(RemotePC, F) ->
     {ok, Bin} = rpc:call(RemotePc, file, read_file, [F]),
     file:write_file(F ++ ".copy", [Bin]).
+
+%% The remote pc is: server@innaky
+% > erl -sname client -setcookie abc
+% > c(myftp).
+% {ok,myftp}
+% > > myftp:get_file(server@innaky, "monitors.erl").
+% ok
