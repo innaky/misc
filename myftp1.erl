417b0d15f95379eb5f9b72d5f83da4535f54ea7e
Innaky <innaky@protonmail.com>
2019-04-08 12:50:19 -0400

    This source code is a infinite loop, i dont see the problem right now, but update this source for adding in the warning zone. I cant callme recursively.

--- myftp1.erl
+++ myftp1.erl
@@ -2,10 +2,20 @@
 -export([get_file/2]).
 
 get_file(RemotePC, File) ->
-    {ok, Data} = rpc:call(RemotePC, file, my_consult, [File]),
-    file:write_file(File ++ ".copy", [Data]).
+    {ok, Data} = rpc:call(RemotePC, myftp1, get_file, [RemotePC, File]),
+    file:write_file(File ++ ".copy", Data, append).
 
 % Helper
+read_step(File) ->
+    case file:open(File, read) of
+	{ok, Fd} ->
+	    Data = my_consult(Fd),
+	    file:close(Fd),
+	    {ok, Data};
+	{error, Why} ->
+	    {error, Why}
+    end.
+
 my_consult(Fd) ->
     case io:read(Fd, '') of
 	{ok, Term} ->
