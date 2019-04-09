eb27ea127c23b25b165d2409ae49ccc9a7ff4db9
Innaky <innaky@protonmail.com>
2019-04-09 13:32:15 -0400

    Adding more logical calling, but i have an error {error,badarg}. And the commentary is an initial ideas about how build my own file transfer logic.

--- myftp1.erl
+++ myftp1.erl
@@ -2,10 +2,10 @@
 -export([get_file/2, read_step/1, my_consult/1]).
 
 get_file(RemotePC, File) ->
-    {ok, Data} = rpc:call(RemotePC, myftp1, get_file, [RemotePC, File]),
-    file:write_file(File ++ ".copy", Data, append).
+    {ok, Data} = rpc:call(RemotePC, myftp1, read_step, [File]),
+    file:write_file(File ++ ".copy", [Data]).
 
-% Helper
+
 read_step(File) ->
     case file:open(File, read) of
 	{ok, Fd} ->
@@ -25,3 +25,35 @@ my_consult(Fd) ->
 	Error ->
 	    Error
     end.
+% This function take an `Start' value, an `End' value and an `Interval' value, and
+% return a list between `Start' to `End' in `Interval' segments.
+% Similar to python [0, 10, 2] = [0, 2, 4, 6, 8, 10]
+% but this function needs to more refinate, because the `End' is append in the last position
+% and not always is correct [0, 12, 5] = [0, 5, 10, 12]
+%segments(Start, End, _Interval) when Start >= End ->
+%    [] ++ [End];
+%segments(Start, End, Interval) ->
+%    [Start | segments(Start+Interval, End, Interval)].
+
+%is_multiple(Num, MultipleOf) ->
+%    Div = Num rem MultipleOf,
+%    if Div =:= 0 ->
+%	    true;
+%       Div =/= 0 ->
+%	    false
+%    end.
+
+%read_step(Filename) ->
+%    case file:open(Filename, [read,binary,raw]) of
+%	{ok, Fd} ->
+%	    Filesize = filelib:file_size(Filename),
+%	    % extract the principal number of Filesize. Max size of a segment of 10 bytes.
+%	    {Big_segment, _} = string:to_integer(integer_to_list(Filesize div 10) ++ "0"),
+%	    % extract the last number of Filesize.
+%	    Last_segment = Filesize rem 10,
+%	    Byte_segment = pread(Fd, 0, 10),
+%	    file:close(Fd),
+%	    {ok, Data};
+%	{error, Why} ->
+%	    {error, Why}
+%    end.
