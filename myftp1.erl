d98c7800c54f44ac1c58745bda9ff4e2ad572e61
Innaky <innaky@protonmail.com>
2019-04-11 02:52:45 -0400

    With this source i can obtain the sustainable_read/1 of the remote file of the an erlang node. get_file/2: Call the remote information of the remote filename (remote erlang node). segments/3: Build a list, from Start, to End in Interval steps. sustainable_read/1: open a file, obtain the Filesize, process the Big_segment of the Filesize an the Small_segment, call the function segments and read the first 10 bytes of informatio, the return data is a list with this information.

--- myftp1.erl
+++ myftp1.erl
@@ -3,19 +3,19 @@
 -export([get_file/2, segments/3, sustainable_read/1]).
 
 get_file(RemotePC, Filename) ->
-    {ok, Fd} = rpc:call(RemotePC, myftp1, sustainable_read, [Filename]),
-    Filesize = filelib:file_size(Filename),
-    {Big_segment, _} = string:to_integer(integer_to_list(Filesize div 10) ++ "0"),
-    Small_segment = Filesize rem 10,
-    List_segments = segments(0, Big_segment, 10),
-    {ok, Bin} = file:pread(Fd, 0, 10),
-    file:pwrite(Fd, 0, Bin),
-    file:close(Fd).
+    rpc:call(RemotePC, myftp1, sustainable_read, [Filename]).
 
 segments(Start, End, _Interval) when Start >= End ->
     [] ++ [End];
 segments(Start, End, Interval) ->
     [Start | segments(Start+Interval, End, Interval)].
 
-sustainable_read(FileName) ->
-    file:open(FileName, [read,binary,raw]).
+sustainable_read(Filename) ->
+    {ok, Fd} = file:open(Filename, [read,binary,raw]),
+    Filesize = filelib:file_size(Filename),
+    {Big_segment, _} = string:to_integer(integer_to_list(Filesize div 10) ++ "0"),
+    Small_segment = Filesize rem 10,
+    List_segments = segments(0, Big_segment, 10),
+    {ok, Bin} = file:pread(Fd, 0, 10),
+    file:close(Fd),
+    [Filesize, Big_segment, Small_segment, List_segments, Bin].
