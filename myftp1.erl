3bbc2153f7c6845f61101e28d0591c65d7ad5784
Innaky <innaky@protonmail.com>
2019-04-10 23:39:39 -0400

    Adding a logic for read an small file and create a file with this information (in construction). segment/3 accept three numbers and generate a list [0, 10, 2] = [0, 2, 4, 6, 8, 10] (in construction) sustainable_read/1 only open a file.

--- myftp1.erl
+++ myftp1.erl
@@ -1,59 +1,21 @@
 -module(myftp1).
--export([get_file/2, read_step/1, my_consult/1]).
 
-get_file(RemotePC, File) ->
-    {ok, Data} = rpc:call(RemotePC, myftp1, read_step, [File]),
-    file:write_file(File ++ ".copy", [Data]).
+-export([get_file/2, segments/3, sustainable_copy/1, read_step/1]).
 
+get_file(RemotePC, Filename) ->
+    {ok, Fd} = rpc:call(RemotePC, myftp1, sustainable_read, [Filename]),
+    Filesize = filelib:file_size(Filename),
+    {Big_segment, _} = string:to_integer(integer_to_list(Filesize div 10) ++ "0"),
+    Small_segment = Filesize rem 10,
+    List_segments = segments(0, Big_segment, 10),
+    {ok, Bin} = file:pread(Fd, 0, 10),
+    file:pwrite(Fd, 0, Bin),
+    file:close(Fd).
 
-read_step(File) ->
-    case file:open(File, read) of
-	{ok, Fd} ->
-	    Data = my_consult(Fd),
-	    file:close(Fd),
-	    {ok, Data};
-	{error, Why} ->
-	    {error, Why}
-    end.
+segments(Start, End, _Interval) when Start >= End ->
+    [] ++ [End];
+segments(Start, End, Interval) ->
+    [Start | segments(Start+Interval, End, Interval)].
 
-my_consult(Fd) ->
-    case io:read(Fd, '') of
-	{ok, Term} ->
-	    [Term|my_consult(Fd)];
-	eof ->
-	    [];
-	Error ->
-	    Error
-    end.
-% This function take an `Start' value, an `End' value and an `Interval' value, and
-% return a list between `Start' to `End' in `Interval' segments.
-% Similar to python [0, 10, 2] = [0, 2, 4, 6, 8, 10]
-% but this function needs to more refinate, because the `End' is append in the last position
-% and not always is correct [0, 12, 5] = [0, 5, 10, 12]
-%segments(Start, End, _Interval) when Start >= End ->
-%    [] ++ [End];
-%segments(Start, End, Interval) ->
-%    [Start | segments(Start+Interval, End, Interval)].
-
-%is_multiple(Num, MultipleOf) ->
-%    Div = Num rem MultipleOf,
-%    if Div =:= 0 ->
-%	    true;
-%       Div =/= 0 ->
-%	    false
-%    end.
-
-%read_step(Filename) ->
-%    case file:open(Filename, [read,binary,raw]) of
-%	{ok, Fd} ->
-%	    Filesize = filelib:file_size(Filename),
-%	    % extract the principal number of Filesize. Max size of a segment of 10 bytes.
-%	    {Big_segment, _} = string:to_integer(integer_to_list(Filesize div 10) ++ "0"),
-%	    % extract the last number of Filesize.
-%	    Last_segment = Filesize rem 10,
-%	    Byte_segment = pread(Fd, 0, 10),
-%	    file:close(Fd),
-%	    {ok, Data};
-%	{error, Why} ->
-%	    {error, Why}
-%    end.
+sustainable_read(FileName) ->
+    file:open(FileName, [read,binary,raw]).
