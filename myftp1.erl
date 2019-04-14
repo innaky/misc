ab2ed6f422038adfb1140e7325e5835b86ed0b54
Innaky <innaky@protonmail.com>
2019-04-13 23:31:54 -0400

    The rpc function call a small segment of the remote file.

--- myftp1.erl
+++ myftp1.erl
@@ -1,21 +1,20 @@
 -module(myftp1).
+-export([get_file/3, segments/3, sustainable_read/2]).
 
--export([get_file/2, segments/3, sustainable_read/1]).
-
-get_file(RemotePC, Filename) ->
-    rpc:call(RemotePC, myftp1, sustainable_read, [Filename]).
+get_file(RemotePC, Filename, Num) ->
+    rpc:call(RemotePC, myftp1, sustainable_read, [Filename, Num]).
 
 segments(Start, End, _Interval) when Start >= End ->
     [] ++ [End];
 segments(Start, End, Interval) ->
     [Start | segments(Start+Interval, End, Interval)].
 
-sustainable_read(Filename) ->
+sustainable_read(Filename, Num) ->
     {ok, Fd} = file:open(Filename, [read,binary,raw]),
     Filesize = filelib:file_size(Filename),
     {Big_segment, _} = string:to_integer(integer_to_list(Filesize div 10) ++ "0"),
     Small_segment = Filesize rem 10,
     List_segments = segments(0, Big_segment, 10),
-    {ok, Bin} = file:pread(Fd, 0, 10),
+    {ok, Bin} = file:pread(Fd, Num, 10),
     file:close(Fd),
     [Filesize, Big_segment, Small_segment, List_segments, Bin].
