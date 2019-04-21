21456a4d0f5db9ca7b7719b48c747ac27ea83628
Innaky <innaky@protonmail.com>
2019-04-21 14:55:07 -0400

    Spawning the functions in client-server mode. usage: 1> c(file_md5). {ok,file_md5} 2> Pid = file_md5:start(). <0.87.0> 3> file_md5:ask(Pid, {files, "."}). [{"simple_server.erl", <<7,222,231,52,137,179,4,247,245,141,85,162,198,83,163,88>>}, {"record.hrl", <<3,71,221,203,225,79,130,63,216,107,157,229,72,68,198,75>>}, {[...],...}, {...}|...]

--- file_md5.erl
+++ file_md5.erl
@@ -1,5 +1,29 @@
 -module(file_md5).
+-export([start/0, server/0, ask/2]).
 -export([md5_small_file/1, files_md5/1, files/1]).
+
+start() ->
+    spawn(?MODULE, server, []).
+
+ask(Pid, Ask) ->
+    client(Pid, Ask).
+
+client(Pid, Request) ->
+    Pid ! {self(), Request},
+    receive
+	{Pid, Response} ->
+	    Response
+    end.
+
+server() ->
+    receive
+	{FromPid, {files, Dir}} ->
+	    FromPid ! {self(), files(Dir)},
+	    server();
+	{FromPid, Other} ->
+	    FromPid ! {self(), {error, Other}},
+	    server()
+    end.
 
 md5_small_file(File) ->
     case filelib:file_size(File) of
