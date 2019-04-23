82226c347d7b0e1c5bc3690377001f2f18f6513b
Innaky <innaky@protonmail.com>
2019-04-22 23:04:49 -0400

    first socket client in Erlang :D

---
+++ get_url.erl
@@ -0,0 +1,18 @@
+-module(get_url).
+-export([nano_get_url/0, receive_data/2]).
+
+nano_get_url() ->
+    nano_get_url("www.google.com").
+
+nano_get_url(Host) ->
+    {ok, Socket} = gen_tcp:connect(Host, 80, [binary, {packet, 0}]),
+    ok = gen_tcp:send(Socket, "GET / HTTP/1.0\r\n\r\n"),
+    receive_data(Socket, []).
+
+receive_data(Socket, SoFar) ->
+    receive
+	{tcp, Socket, Bin} ->
+	    receive_data(Socket, [Bin|SoFar]);
+	{tcp_closed, Socket} ->
+	    erlang:list_to_binary(lists:reverse(SoFar))
+    end.
