c5debcabad7e9339e8ff7c0490f69ab5dc0c1b8e
Innaky <innaky@protonmail.com>
2019-10-11 23:24:50 -0400

    new function fact
    Adding the factorial function

--- recursion.exs
+++ recursion.exs
@@ -1,4 +1,7 @@
 defmodule Recursion do
   def my_map(_func, []), do: []
   def my_map(func, [head|tail]), do: [func.(head) | my_map(func, tail)]
+  
+  def fact(0), do: 1
+  def fact(x), do: x * fact(x - 1)
 end
