2859d6547846c4fbba3829b9c1bda1ad717db37d
Innaky <innaky@protonmail.com>
2019-10-12 05:37:43 -0400

    recursion.exs
    * my_length/1: Return a length of a list
    * repeat/2: Return a list of repeat element `n' times.
    * reverse/1: Return a reverse of a list.

--- recursion.exs
+++ recursion.exs
@@ -4,4 +4,15 @@ defmodule Recursion do
   
   def fact(0), do: 1
   def fact(x), do: x * fact(x - 1)
+  
+  def my_length([]), do: 0
+  def my_length([_|cdr], do: 1 + my_length(cdr)
+  
+  # return a list of repeat element `n' times.
+  def repeat(_, 0), do: []
+  def repeat(elem, times), do: [elem | repeat(elem, times - 1)]
+  
+  # return a reverse of a list
+  def reverse([]), do: []
+  def reverse([head|tail]), do: tail ++ [head]
 end
