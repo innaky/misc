827524fd59f344d551c67f4e04e7630c35129f70
Innaky <innaky@protonmail.com>
2020-06-20 22:56:11 -0400

    (nw_fn) sublst Transform all elements of the list in a list.

--- recursion.exs
+++ recursion.exs
@@ -6,7 +6,7 @@ defmodule Recursion do
   def fact(x), do: x * fact(x - 1)
   
   def my_length([]), do: 0
-  def my_length([_|cdr], do: 1 + my_length(cdr)
+  def my_length([_|cdr]), do: 1 + my_length(cdr)
   
   # return a list of repeat element `n' times.
   def repeat(_, 0), do: []
@@ -20,4 +20,7 @@ defmodule Recursion do
   def zip(_, []), do: []
   def zip([], _), do: []
   def zip([hx|tx], [hy|ty]), do: [{hx, hy}] ++ zip(tx, ty)
+
+  def sublst([]), do: []
+  def sublst([h|t]), do: [[h]|sublst(t)]
 end
